5998a4285eb1bffbd6852120154b62c60e2d00a3
Nizar7zak <nizar7zakout@gmail.com>
2021-08-29 11:22:48 +0300

    create server and router Relates #3

---
+++ server/app.js
@@ -0,0 +1,23 @@
+require('env2')('.env');
+const express = require('express');
+const compression = require('compression');
+const cookieParser = require('cookie-parser');
+
+const { join } = require('path');
+const router = require('./router');
+
+const app = express();
+
+app.set('PORT', process.env.PORT || '3333');
+app.disable('x-powered-by');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(compression());
+app.use(cookieParser());
+
+app.use(express.static(join(__dirname, '..', 'public')));
+
+app.use(router);
+
+module.exports = app;
---
+++ server/index.js
@@ -0,0 +1,6 @@
+const app = require('./app');
+
+app.listen(app.get('PORT'), () => {
+  // eslint-disable-next-line no-console
+  console.log(' server is running http://localhost:2222');
+});
