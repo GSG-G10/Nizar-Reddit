0ac4f75e90b3aee6d38e855354ccced984e40dce
Nizar7zak <nizar7zakout@gmail.com>
2021-08-29 13:46:40 +0300

    prepare function to bulid database for test db relates #5

---
+++ server/dataBase/build/bulid.js
@@ -0,0 +1,11 @@
+const { join } = require('path');
+const { readFileSync } = require('fs');
+const connection = require('../config/connection');
+
+const sql = readFileSync(join(__dirname, 'init.sql'));
+
+const dbBuild = () => {
+  connection.query(sql);
+};
+
+module.exports = dbBuild;
