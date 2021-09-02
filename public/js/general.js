c9f9a3be49cfe32b5aaca7d5ee00f7e02fd7a46f
Nizar7zak <nizar7zakout@gmail.com>
2021-09-02 04:58:55 +0300

    handle comment text area based on status user or visitor relates #35

--- public/js/general.js
+++ public/js/general.js
@@ -1,14 +1,20 @@
 const cookies = document.cookie;
 const notAuthBar = document.querySelector('.not-auth');
 const authBar = document.querySelector('.auth');
+const authBar1 = document.querySelector('.auth1');
+const notAuthBar1 = document.querySelector('.not-auth1');
 
 const user = () => {
   notAuthBar.style.display = 'none';
   authBar.style.display = 'flex';
+  authBar1.style.display = 'flex';
+  notAuthBar1.style.display = 'none';
 };
 const visitor = () => {
   notAuthBar.style.display = 'flex';
   authBar.style.display = 'none';
+  authBar1.style.display = 'none';
+  notAuthBar1.style.display = 'flex';
 };
 
 const checkCookie = (coo) => {
--- public/js/singlepageDom.js
+++ public/js/singlepageDom.js
@@ -1,12 +1,21 @@
 const createTextArea = () => {
   const container = document.querySelector('.popular1');
   const contianerComments = bulidElement('div', container, 'continer-comments');
-  const addComment = bulidElement('div', contianerComments, 'add-comment');
+
+  const addComment = bulidElement('div', contianerComments, 'auth1');
   const textArea = bulidElement('textarea', addComment, 'add-text');
   textArea.setAttribute('name', 'comment');
   const commentBtn = bulidElement('button', addComment, 'comment-button');
   commentBtn.textContent = 'comment';
   commentBtn.setAttribute('href', '/addcomment');
+
+  const notAuth = bulidElement('div', contianerComments, 'not-auth1');
+  const containerToLogin = bulidElement('div', notAuth, 'container-login');
+  const msgToLogin = bulidElement('p', containerToLogin, 'msg-login');
+  msgToLogin.textContent = 'Do you want to add comment? well login to Reddit';
+  const btnLogien = bulidElement('a', containerToLogin, 'btn-login1');
+  btnLogien.setAttribute('href', '/login-page');
+  btnLogien.textContent = 'login';
 };
 createTextArea();
 
@@ -14,10 +23,13 @@ const buildComment = (username, content) => {
   const contianerComments = document.querySelector('.continer-comments');
   const commentPart = bulidElement('div', contianerComments, 'comment-part');
   const userInfo = bulidElement('div', commentPart, 'user-commented');
+
   const photoUser = bulidElement('img', userInfo, 'img-user');
   photoUser.src = './icons/user.svg';
+
   const userName = bulidElement('p', userInfo, 'user-name1');
   userName.textContent = username;
+
   const contentComment = bulidElement('p', commentPart, 'comment-content');
   contentComment.textContent = content;
 };
